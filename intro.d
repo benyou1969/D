0577a58e1ec0b6d797ec35e79b052b783fd7b96b
benyou1969 <benyou.11@hotmail.com>
2019-07-04 09:38:49 +0100

    Characters note: when you use character try to type it inside ' '

--- intro.d
+++ intro.d
@@ -12,12 +12,8 @@ import std.stdio;
 import std.range;
 
 void main(){
-      int numbr_1 = 1;
-      auto booleanTrue = true;
-      bool snowy_day = false;
-      writeln("boolean is: ", booleanTrue, " . \n Is it a snowy_day: ", snowy_day);
-      writeln("boolean max: ", bool.max);
-      writeln("boolean min: ", bool.min);
-      writefln("boolean: %s", true);
-
+      writeln("min char: ", char.min);
+      writeln("max char: ", char.max);
+      char b = 'B';
+      writefln("boolean: %c", b);
 }
