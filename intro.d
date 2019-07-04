9b429bb74a64f0b3b2d7b321447ab476bc97cc16
benyou1969 <benyou.11@hotmail.com>
2019-07-04 19:09:50 +0100

    convert string to integer or the opposite

--- intro.d
+++ intro.d
@@ -9,12 +9,25 @@
       and void the means that the function doesn't return anything
 */
 import std.stdio;
+import std.conv;
+import std.math;
+import std.random;
+import std.datetime;
+import std.algorithm;
+import std.array;
+import std.string;
+import std.format;
+import std.typecons : No;
+import std.typecons;
+import std.range : chain;
+import std.file;
+import core.thread;
+import std.parallelism;
+import std.concurrency;
 import std.range;
-
 void main(){
-      int cInteger = 15;
-      short cShort = cast(short) cInteger;
-      short cShort1 = cast(short) 144444;
-      writeln("short : ",cShort1);
-
+      auto sInt = to!string(20);
+      writeln("sInt is: " ,typeof(sInt).stringof);
+      auto iStr = to!int("10");
+      writeln("iStr is: " ,typeof(iStr).stringof);
 }
