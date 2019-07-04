c9be9cd78373e594b8faaee2b7c278112cb350ef
benyou1969 <benyou.11@hotmail.com>
2019-07-04 09:56:01 +0100

    integer and floating numbers

--- intro.d
+++ intro.d
@@ -12,8 +12,23 @@ import std.stdio;
 import std.range;
 
 void main(){
-      writeln("min char: ", char.min);
-      writeln("max char: ", char.max);
-      char b = 'B';
-      writefln("boolean: %c", b);
+      writeln("byte min: ", byte.min);
+      writeln("byte max: ", byte.max);
+      writeln("short min: ", short.min);
+      writeln("short max: ", short.max);
+      writeln("long min: ", long.min);
+      writeln("long max: ", long.max);
+      writeln("int min: ", int.min);
+      writeln("int max: ", int.max);
+      // all type of numbers above when we format them we use %d
+      long number = 196_966_599;
+      writefln("long number is: %15d", number);
+
+      // float when we format them we use %f
+      writeln("float max: ", float.max);
+      float first_num = 3.333333333333;
+      float second_num = 3.333333333333;
+      writefln("Sum: %10.3f", first_num + second_num);
+      // first number after the percentage % is right shifting (space)
+      // second number after the percentage % is for the how numbers with show after the dot (.XXX)      
 }
